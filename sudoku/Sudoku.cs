1b5680724907aaceb162c061f5c0dae8b3ef5108
jokerhero <jokerhero9@yahoo.com>
2015-03-24 16:39:21 -0700

    Added Sudoku class

---
+++ sudoku/Sudoku.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sudoku
+{
+    class Sudoku
+    {
+        String puzzleAsString;
+        int[,] puzzleAsArray;
+
+        public Sudoku()
+        {
+            puzzleAsString = "";
+            puzzleAsArray = new int[9, 9];
+        }
+
+        public Sudoku(String puzzle)
+        {
+            puzzleAsString = puzzle;
+        }
+
+        public Sudoku(int[,] puzzle)
+        {
+            puzzleAsArray = puzzle;
+        }
+    }
+}
